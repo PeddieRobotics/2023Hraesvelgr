806a8508ad7f252b73aaed462855f6c86920594c
Ashwin Narayan <anarayan-26@peddie.org>
2023-01-21 12:42:50 -0500

    Started Arm Skeleton Code

--- 2023-H/src/main/java/frc/robot/subsystems/Arm.java
+++ 2023-H/src/main/java/frc/robot/subsystems/Arm.java
@@ -1,5 +1,43 @@
 package frc.robot.subsystems;
 
-public class Arm {
-    //tbd
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Arm extends SubsystemBase {
+    private static Arm instance;
+
+    // TODO: elbow, only extends once at the begining of the game
+
+    private CANSparkMax shoulderMotor, wristMotor, intakeMotor;
+
+    // TODO: constructor
+
+    public void setIntakeSpeed(double intakeSpeed) {
+        intakeMotor.set(intakeSpeed);
+    }
+
+    public double getIntakeSpeed() {
+        return intakeMotor.get();
+    }
+
+    public void stopIntake() {
+        // TODO: add shoulder lifts and stuff
+        intakeMotor.set(0);
+    }
+
+    public double getIntakeOutputCurrent() {
+        return intakeMotor.getOutputCurrent();
+    }
+
+    public double getIntakeMotorTemperature() {
+        return intakeMotor.getMotorTemperature();
+    }
+
+    public static Arm getInstance() {
+        if(instance==null) {
+            instance=new Arm();
+        }
+        return instance;
+    }
 }
