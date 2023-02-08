ae62c2354ca99a5c69935ce4f075a71195fbf1e1
anthonymorales91011 <93945633+anthonymorales91011@users.noreply.github.com>
2023-02-08 15:30:27 -0500

    fix

--- 2023-H/src/main/java/frc/robot/subsystems/Shuffleboard.java
+++ 2023-H/src/main/java/frc/robot/subsystems/Shuffleboard.java
@@ -7,70 +7,26 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.subsystems.Autonomous.Autonomous;
 import frc.robot.utils.Constants;
 
 public class Shuffleboard extends SubsystemBase{
     public static Shuffleboard shuffleboard;
-    private Autonomous autonomous;
     private Drivetrain drivetrain;
     private Claw claw;
     private Shoulder shoulder;
     private Wrist wrist;
 
-    private SendableChooser<String> startingGamePieceSelector, startingColumnSelector, scoringLevelSelector, objectiveOneSelector, objectiveTwoSelector, objectiveThreeSelector, objectiveFourSelector;
-
-    public static final String Cone = "Cone";
-    public static final String Cube = "Cube";
-
-    public static final String ColumnOne = "Column One";
-    public static final String ColumnTwo = "Column Two";
-    public static final String ColumnThree = "Column Three";
-    public static final String ColumnFour = "Column Four";
-    public static final String ColumnFive = "Column Five";
-    public static final String ColumnSix = "Column Six";
-    public static final String ColumnSeven = "Column Seven";
-    public static final String ColumnEight = "Column Eight";
-    public static final String ColumnNine = "Column Nine";
-
-    public static final String ScoringLevelOne = "Scoring Level One";
-    public static final String ScoringLevelTwo = "Scoring Level Two";
-    public static final String ScoringLevelThree = "Scoring Level Three";
-
-    public static final String LeaveCommunity = "Leave Community";
-    public static final String Balance = "Balance";
-    public static final String GrabOne = "Grab One";
-    public static final String ScoreOne = "Score One";
-    public static final String Prepare = "Prepare";
-
-    public static final String None = "Null";
-
-    public static boolean Inversion = true;
-
-
-
 
     public Shuffleboard() {
-        startingGamePieceSelector = new SendableChooser<String>();
-        startingColumnSelector = new SendableChooser<String>();
-        scoringLevelSelector = new SendableChooser<String>();
-        objectiveOneSelector = new SendableChooser<String>();
-        objectiveTwoSelector = new SendableChooser<String>();
-        objectiveThreeSelector = new SendableChooser<String>();
-        objectiveFourSelector = new SendableChooser<String>();
-
-
         drivetrain = Drivetrain.getInstance();
         claw = Claw.getInstance();
         shoulder = Shoulder.getInstance();
         wrist = Wrist.getInstance();
-        autonomous = autonomous.getInstance();
 
         drivetrainShuffleboard();
         clawShuffleboard();
         shoulderShuffleboard();
         wristShuffleboard();
-        autonomousShuffleboard();
     }
 
     public static Shuffleboard getInstance(){
@@ -86,38 +42,6 @@ public class Shuffleboard extends SubsystemBase{
         clawShuffleboard();
         shoulderShuffleboard();
         wristShuffleboard();
-        autonomousShuffleboard();
-    }
-
-    public String returnStartingGamePieceCommand() {
-        return startingGamePieceSelector.getSelected();
-    }
-
-    public String returnStartingColumnCommand() {
-        return startingColumnSelector.getSelected();
-    }
-
-    public String returnScoringLevelCommand() {
-        return startingGamePieceSelector.getSelected();
-    }
-
-    public boolean returnIsInvertedCommand() {
-        return Inversion;
-    }
-    public String returnObjectiveOneCommand() {
-        return objectiveOneSelector.getSelected();
-    }
-
-    public String returnObjectiveTwoCommand() {
-        return objectiveTwoSelector.getSelected();
-    }
-
-    public String returnObjectiveThreeCommand() {
-        return objectiveThreeSelector.getSelected();
-    }
-
-    public String returnObjectiveFourCommand() {
-        return objectiveFourSelector.getSelected();
     }
 
     private void drivetrainShuffleboard(){
@@ -190,31 +114,5 @@ public class Shuffleboard extends SubsystemBase{
         SmartDashboard.putNumber("wrist velocity", wrist.getVelocity());
         SmartDashboard.putNumber("wrist Motor Current", wrist.getOutputCurrent());
         SmartDashboard.putNumber("wrist Motor temperature", wrist.getMotorTemperature());
-    }
-
-    private void autonomousShuffleboard() {
-        startingGamePieceSelector.setDefaultOption("Cone", Cone);
-        startingGamePieceSelector.addOption("Cube", Cube);
-        SmartDashboard.putData("Starting Game Piece", startingGamePieceSelector);
-
-        startingColumnSelector.setDefaultOption("Column 1", ColumnOne);
-        SmartDashboard.putData("Starting Column", startingColumnSelector);
-
-        scoringLevelSelector.setDefaultOption("Level 1", ScoringLevelOne);
-        SmartDashboard.putData("Scoring level", scoringLevelSelector);
-
-        SmartDashboard.putBoolean("Is Inverted", Inversion);
-
-        objectiveOneSelector.setDefaultOption("Nothing", None);
-        SmartDashboard.putData("Objective One", objectiveOneSelector);
-
-        objectiveTwoSelector.setDefaultOption("Nothing", None);
-        SmartDashboard.putData("Objective Two", objectiveTwoSelector);
-
-        objectiveThreeSelector.setDefaultOption("Nothing", None);
-        SmartDashboard.putData("Objective Three", objectiveThreeSelector);
-
-        objectiveFourSelector.setDefaultOption("Nothing", None);
-        SmartDashboard.putData("Objective Four", objectiveFourSelector);
     }
 }
