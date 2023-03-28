39d15ec52593cde8a8b83fd1105331c74c7dee7f
anthonymorales91011 <93945633+anthonymorales91011@users.noreply.github.com>
2023-03-28 16:50:27 -0400

    logging

--- 2023-H/src/main/java/frc/robot/Robot.java
+++ 2023-H/src/main/java/frc/robot/Robot.java
@@ -4,6 +4,13 @@
 
 package frc.robot;
 
+import org.littletonrobotics.junction.LogFileUtil;
+import org.littletonrobotics.junction.LoggedRobot;
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.NT4Publisher;
+import org.littletonrobotics.junction.wpilog.WPILOGReader;
+import org.littletonrobotics.junction.wpilog.WPILOGWriter;
+
 import com.pathplanner.lib.server.PathPlannerServer;
 import com.revrobotics.CANSparkMax.IdleMode;
 
@@ -15,7 +22,6 @@ import edu.wpi.first.wpilibj.PowerDistribution;
 // import org.littletonrobotics.junction.io.ByteLogReceiver;
 // import org.littletonrobotics.junction.io.LogSocketServer;
 
-import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.PowerDistribution.ModuleType;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -40,7 +46,7 @@ import frc.robot.utils.Constants.OIConstants;
  * build.gradle file in the
  * project.
  */
-public class Robot extends TimedRobot {
+public class Robot extends LoggedRobot {
     private Command autonomousCommand;
     private static RobotContainer robotContainer;
     private ShuffleboardMain shuffleboard;
@@ -49,30 +55,45 @@ public class Robot extends TimedRobot {
 
     @Override
     public void robotInit() {
-        LiveWindow.setEnabled(false);
+        Logger.getInstance().recordMetadata("ProjectName", "MyProject"); // Set a metadata value
 
-        robotContainer = new RobotContainer();
+        if (isReal()) {
+        Logger.getInstance().addDataReceiver(new WPILOGWriter("/media/sda1/")); // Log to a USB stick
+        Logger.getInstance().addDataReceiver(new NT4Publisher()); // Publish data to NetworkTables
+        //new PowerDistribution(1, ModuleType.kRev); // Enables power distribution logging
+    } else {
+        setUseTiming(false); // Run as fast as possible
+        String logPath = LogFileUtil.findReplayLog(); // Pull the replay log from AdvantageScope (or prompt the user)
+        Logger.getInstance().setReplaySource(new WPILOGReader(logPath)); // Read replay log
+        Logger.getInstance().addDataReceiver(new WPILOGWriter(LogFileUtil.addPathSuffix(logPath, "_sim"))); // Save outputs to a new log
+    }
 
-        shuffleboard = ShuffleboardMain.getInstance();
-        if(OIConstants.kUseDebugModeLayout){
-            // Set up a REV PDH in order to get key status information
-            // pdh = new PowerDistribution(1, ModuleType.kRev);
+    // Logger.getInstance().disableDeterministicTimestamps() // See "Deterministic Timestamps" in the "Understanding Data Flow" page
+    Logger.getInstance().start(); // Start logging! No more data receivers, replay sources, or metadata values may be added.
+    LiveWindow.setEnabled(false);
 
-            shuffleboard.setupDebugMode();
-            shuffleboard.setupAutoSelector();
-        }
-        else{
-            shuffleboard.setupCompetitionMode();
-            shuffleboard.setupAutoSelector();
-        }
+    robotContainer = new RobotContainer();
 
-        PathPlannerServer.startServer(5985); //SHOULD BE 5985!!!!! 5895 WILL NOT WORK!!!!!
-        SmartDashboard.putData(CommandScheduler.getInstance());
+    shuffleboard = ShuffleboardMain.getInstance();
+    if(OIConstants.kUseDebugModeLayout){
+        // Set up a REV PDH in order to get key status information
+        // pdh = new PowerDistribution(1, ModuleType.kRev);
 
-        ranAutonomousRoutine = false;
+        shuffleboard.setupDebugMode();
+        shuffleboard.setupAutoSelector();
+    }
+    else{
+        shuffleboard.setupCompetitionMode();
+        shuffleboard.setupAutoSelector();
+    }
 
-        LimelightFront.getInstance().setPipeline(7);
-        LimelightBack.getInstance().setPipeline(0);
+    PathPlannerServer.startServer(5985); //SHOULD BE 5985!!!!! 5895 WILL NOT WORK!!!!!
+    SmartDashboard.putData(CommandScheduler.getInstance());
+
+    ranAutonomousRoutine = false;
+
+    LimelightFront.getInstance().setPipeline(7);
+    LimelightBack.getInstance().setPipeline(0);
     }
 
     @Override
